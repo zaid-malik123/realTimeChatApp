07f662cc894a0b3c7fe6bf5910e8c87f4df8fda7
Zaid Malik <zaid malik>
2025-07-27 22:33:54 +0530

    cookie-change

--- backend/config/Token.js
+++ backend/config/Token.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 
 module.exports.genToken = async (userId)=>{
 try {
-   const token = await jwt.sign({userId}, process.env.JWT_SECRET,{expiresIn:"1d"});
+   const token = await jwt.sign({userId}, process.env.JWT_SECRET,{expiresIn:"2d"});
    return token
 } catch (error) {
     console.log("token error --->" ,error)
